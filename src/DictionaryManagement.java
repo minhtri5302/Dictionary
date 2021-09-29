84ceabdf52cea1713b3bf830557bbf747118235a
minhtriuet <minhtrithcslqd1@gmail.com>
2021-09-29 21:09:38 +0700

    Update DictionaryManagement.java - basic version.

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class DictionaryManagement {
+    private Dictionary s = new Dictionary();
+    public void insertFromCommandline() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; ++i)
+        {
+            String a = sc.nextLine();
+            String b = sc.nextLine();
+            s.a[i] = new Word(a, b);
+        }
+    }
 }
