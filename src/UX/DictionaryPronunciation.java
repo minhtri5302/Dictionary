9533ced525920b7b8d3b8400ea2e47c197c05412
minhtriuet <minhtrithcslqd1@gmail.com>
2021-10-16 00:07:37 +0700

    Add textToSpeech.

--- src/UX/DictionaryPronunciation.java
+++ src/UX/DictionaryPronunciation.java
@@ -1,5 +1,14 @@
 package UX;
 
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
 public class DictionaryPronunciation {
 
+    public void textToSpeech(String text){
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        Voice voice = VoiceManager.getInstance().getVoice("kevin16");
+        voice.allocate();
+        voice.speak(text);
+        voice.deallocate();
+    }
 }
